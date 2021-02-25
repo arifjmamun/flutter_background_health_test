77b67870a2a99b0a5932754a3d760a83cc89d27e
Md. Ariful Islam <arifjmamun24@gmail.com>
2021-02-25 14:30:27 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/arif/dev/background/MainActivity.kt
@@ -0,0 +1,23 @@
+package arif.dev.background
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry;
+
+
+class MainActivity: FlutterActivity() {
+    // override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    //     // GeneratedPluginRegistrant.registerWith(flutterEngine);
+    //   var shimPluginRegistry = ShimPluginRegistry(flutterEngine)
+    //   flutterEngine.getPlugins().add(io.flutter.plugins.deviceinfo.DeviceInfoPlugin())
+    //   flutterEngine.getPlugins().add(com.amolg.flutterbarcodescanner.FlutterBarcodeScannerPlugin())
+    //   flutterEngine.getPlugins().add(io.flutter.plugins.flutter_plugin_android_lifecycle.FlutterAndroidLifecyclePlugin())
+    //   flutterEngine.getPlugins().add(io.flutter.plugins.pathprovider.PathProviderPlugin())
+    //   flutterEngine.getPlugins().add(io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin())
+    //   flutterEngine.getPlugins().add(be.tramckrijte.workmanager.WorkmanagerPlugin())
+    //   // cachet.plugins.health.HealthPlugin.registerWith(shimPluginRegistry.registrarFor("cachet.plugins.health.HealthPlugin"))
+    // }
+}
